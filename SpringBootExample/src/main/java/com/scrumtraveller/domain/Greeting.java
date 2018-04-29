f04d073d34017d8e90e5669d1a26068755a6a5c7
Cheehong Hsia <hsia.cheehong@gmail.com>
2018-04-29 15:03:01 +0200

    Package war solved!

--- SpringBootExample/src/main/java/com/scrumtraveller/domain/Greeting.java
+++ SpringBootExample/src/main/java/com/scrumtraveller/domain/Greeting.java
@@ -1,4 +1,4 @@
-package domain;
+package com.scrumtraveller.domain;
 
 
 public class Greeting {
--- SpringBootExample/src/main/java/com/scrumtraveller/springboot/controller/GreetingController.java
+++ SpringBootExample/src/main/java/com/scrumtraveller/springboot/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.scrumtraveller.springboot.controller;
 
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import domain.Greeting;
+import com.scrumtraveller.domain.Greeting;
 
 @RestController
 public class GreetingController {
