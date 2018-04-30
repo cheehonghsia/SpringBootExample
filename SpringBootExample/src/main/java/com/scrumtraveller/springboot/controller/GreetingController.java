fa1f023677605582c91a232ad755645371097536
Cheehong Hsia <hsia.cheehong@gmail.com>
2018-04-30 22:57:11 +0200

    Added extra method to deal with param

--- SpringBootExample/src/main/java/com/scrumtraveller/springboot/controller/GreetingController.java
+++ SpringBootExample/src/main/java/com/scrumtraveller/springboot/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.scrumtraveller.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scrumtraveller.springboot.domain.Greeting;
@@ -13,9 +15,18 @@ public class GreetingController {
 	@Autowired
 	private IGreetingService greetingService;
 
-	@RequestMapping("/greeting")
+	@RequestMapping(value = "/greeting", params = { "name" }, method = RequestMethod.GET)
+	public Greeting greetingWithName(@RequestParam(value = "name", required = false) String name) {
+
+		return this.greetingService.getGreeting(name);
+
+	}
+
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
 	public Greeting greeting() {
+
 		return this.greetingService.getGreeting();
 
 	}
+
 }
